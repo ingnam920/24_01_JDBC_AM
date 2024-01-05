68bf57b8c4636b14eb206517893b547f774ab4d3
sunghyun <ingnamc920@gmail.com>
2024-01-05 17:04:14 +0900

    mysql테스트중

--- src/com/KoreaIT/java/JAM/Main.java
+++ src/com/KoreaIT/java/JAM/Main.java
@@ -1,10 +1,102 @@
 package com.KoreaIT.java.JAM;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub	
-		System.out.println(1);
+		System.out.println("==프로그램 시작==");
+		Scanner sc = new Scanner(System.in);
+		List<Article> articles = new ArrayList<>();
+		int lastArticleId = 0;
+		while (true) {
+			System.out.print("명령어 > ");
+			String cmd = sc.nextLine().trim();
+			if (cmd.equals("exit")) {
+				break;
+			}
+			if (cmd.equals("article write")) {
+				System.out.println("==글쓰기==");
+				int id = lastArticleId + 1;
+				System.out.print("제목 : ");
+				String title = sc.nextLine();
+				System.out.print("내용 : ");
+				String body = sc.nextLine();
 
+				
+				Article article = new Article(id, title, body);
+				articles.add(article);
+
+				lastArticleId = id;
+
+				System.out.println(id + "번 글이 등록되었습니다");
+
+				//////////////////////
+
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/JDBC_AM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					String sql = "INSERT INTO article ";
+					sql += "SET regDate = NOW(),";
+					sql += "updateDate = NOW(),";
+					sql += "title = '" + title + "',";
+					sql += "`body`= '" + body + "';";
+
+					System.out.println(sql);
+
+					pstmt = conn.prepareStatement(sql);
+
+					int affectedRow = pstmt.executeUpdate();
+
+					System.out.println("affectedRow : " + affectedRow);
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러 : " + e);
+				} finally {
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+
+
+			} else if (cmd.equals("article list")) {
+				System.out.println("==목록==");
+				if (articles.size() == 0) {
+					System.out.println("게시글이 없습니다");
+					continue;
+				}
+				System.out.println("  번호  /   제목  ");
+				for (Article article : articles) {
+					System.out.printf("  %d     /   %s   \n", article.getId(), article.getTitle());
+				}
+			}
+		}
+		System.out.println("==프로그램 종료==");
+		sc.close();
 	}
-
 }
